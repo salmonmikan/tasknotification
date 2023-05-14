2cfba5c15336e110a77c89a3be6e61b9d10a7c16
Yu yume <yuki2001yuki@gmail.com>
2023-05-14 23:16:08 +0900

    todays commit

--- index.js
+++ index.js
@@ -5,45 +5,53 @@ console.log("Hello World!");
 
 const LAUNCH_OPTION = {
   headless : false
-}
+};
 
 async function main() {
-const browser = await puppeteer.launch(LAUNCH_OPTION);
 try {
-  const USERNAME = process.env.USERNAME;
-  const PASSWORD = process.env.PASSWORD;
+  // login info
+  const USERNAME = process.env.USER_NAME;
+  const PASSWORD = process.env.PASS_WORD;
+  const URL = "https://portal.tku.ac.jp/";
 
-	const page = await browser.newPage();	// 新しいタブを開く
-	
-  await page.goto("https://portal.tku.ac.jp/", {"waitUntil":"domcontentloaded"});
-  //await page.waitForTimeout(5000);
+  // new page, go to TKU login page
+  const browser = await puppeteer.launch(LAUNCH_OPTION);
+	const page = await browser.newPage();
+  await page.goto(URL, {"waitUntil":"networkidle2"});
 
+  // Specify the input location and enter the login information
   const usernameField = await page.$x('/html/body/div/form/table[1]/tbody/tr[1]/td[2]/input');
   await usernameField[0].type(USERNAME);
   const passwordField = await page.$x('/html/body/div/form/table[1]/tbody/tr[2]/td[2]/input');
   await passwordField[0].type(PASSWORD);
-  const signinButton = await page.$x('/html/body/div/form/table[1]/tbody/tr[4]/td/div/input');
-  await signinButton[0].click();
-  await page.waitForNavigation({waitUntil: 'networkidle2'})
+  const loginButton = await page.$x('/html/body/div/form/table[1]/tbody/tr[4]/td/div/input');
+  await loginButton[0].click();
+
+  // Go to manaba, get the new page and click the manaba button
+  await page.waitForNavigation({waitUntil: 'networkidle2'}); 
   const manabaButton = await page.$x('//*[@id="leftside"]/div[2]/div/div/div[1]/a');
   await manabaButton[0].click();
-  await page.waitForNavigation({waitUntil: 'networkidle2'})
 
-  const manabaPage = page[1];
-  await manabaPage.bringToFront();
-  await page.waitForNavigation({waitUntil: 'networkidle2'})
+  // Get the number of courses
+  // const courseList = await page.$$('.courselist-c');
+  // console.log(`履修中：${courseList.length} コマ`);
 
-  const courseList = await manabaPage.$$('.courselist-c');
-  console.log(`履修中：${courseList.length}コマ`);
+  // Go to unsubmittedlist, Get the number of unsubmitted tasks
+  const pagetarget = await browser.waitForTarget(t => t.opener() === page.target());
+  const newpage = await pagetarget.page();
+  const unsubmitted_List_Button = await newpage.$x('//*[@id="container"]/div[2]/div/div[5]/div[1]/div[2]/div/a');
+  await unsubmitted_List_Button[0].click();
+  // await page.goto("https://manaba.tku.ac.jp/ct/home_library_query");
+  // await page.waitForSelector('//*[contains(@claass, "row")]');
+  // const unsubmitted_List = await page.$x('//*[contains(@class, "row")]');
+  // console.log(`未提出課題：${unsubmitted_List.length}個`);
 
-  const unsubmittedListButton = await manabaPage.$x('//*[@id="container"]/div[2]/div/div[5]/div[1]/div[2]/div/a');
-  await unsubmittedListButton[0].click();
-  await manabaPage.waitForSelector('//*[contains(@class, "row")]');
-  const unsubmittedList = await manabaPage.$x('//*[contains(@class, "row")]');
-  console.log(`未提出課題：${unsubmittedList.length}個`);
+} catch(e) {
+  console.log(e);
 
 } finally {
 	await browser.close();
+
 }}
 
 main()
